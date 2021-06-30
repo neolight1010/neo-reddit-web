782d372d2236ce2d4c0ac353e7b3cc207a13cef8
Anthony Suárez <neolight1010@gmail.com>
2021-06-30 13:11:32 -0500

    Render posts in index page.

--- src/generated/graphql.ts
+++ src/generated/graphql.ts
@@ -169,6 +169,17 @@ export type MeQuery = (
   ) }
 );
 
+export type PostsQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type PostsQuery = (
+  { __typename?: 'Query' }
+  & { posts: Array<(
+    { __typename?: 'Post' }
+    & Pick<Post, 'id' | 'title' | 'createdAt' | 'updatedAt'>
+  )> }
+);
+
 export const UsernameAndIdFragmentDoc = gql`
     fragment UsernameAndId on User {
   username
@@ -234,4 +245,18 @@ export const MeDocument = gql`
 
 export function useMeQuery(options: Omit<Urql.UseQueryArgs<MeQueryVariables>, 'query'> = {}) {
   return Urql.useQuery<MeQuery>({ query: MeDocument, ...options });
+};
+export const PostsDocument = gql`
+    query Posts {
+  posts {
+    id
+    title
+    createdAt
+    updatedAt
+  }
+}
+    `;
+
+export function usePostsQuery(options: Omit<Urql.UseQueryArgs<PostsQueryVariables>, 'query'> = {}) {
+  return Urql.useQuery<PostsQuery>({ query: PostsDocument, ...options });
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,24 @@
 import { NavBar } from "../components/NavBar";
 import { withUrqlClient } from "next-urql";
 import { createUrqlClient } from "../utils/createUrqlClient";
+import { Box } from "@chakra-ui/layout";
+import { usePostsQuery } from "../generated/graphql";
 
-const Index = () => (
-  <>
-    <NavBar />
-    <div>Hello</div>
-  </>
-);
+const Index = () => {
+  const [postsData, _execPostsQuery] = usePostsQuery();
 
-export default withUrqlClient(createUrqlClient, { ssr: true })(Index);
+  return (
+    <Box>
+      <NavBar />
+      <Box id="posts">
+        {!postsData.data
+          ? null
+          : postsData.data.posts.map((post) => (
+              <Box key={post.id}>{post.title}</Box>
+            ))}
+      </Box>
+    </Box>
+  );
+};
+
+export default withUrqlClient(createUrqlClient, { ssr: false })(Index);
