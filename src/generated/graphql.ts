79c39f4d1b2aac49d2fd9b0fc405d57fb29bf6ad
Anthony Suárez <neolight1010@gmail.com>
2021-09-20 14:15:57 -0500

    Complete forgotPassword functionality.

--- src/generated/graphql.ts
+++ src/generated/graphql.ts
@@ -151,6 +151,16 @@ export type ChangePasswordMutation = (
   ) }
 );
 
+export type ForgotPasswordMutationVariables = Exact<{
+  email: Scalars['String'];
+}>;
+
+
+export type ForgotPasswordMutation = (
+  { __typename?: 'Mutation' }
+  & Pick<Mutation, 'forgotPassword'>
+);
+
 export type LoginMutationVariables = Exact<{
   username: Scalars['String'];
   password: Scalars['String'];
@@ -243,6 +253,15 @@ export const ChangePasswordDocument = gql`
 
 export function useChangePasswordMutation() {
   return Urql.useMutation<ChangePasswordMutation, ChangePasswordMutationVariables>(ChangePasswordDocument);
+};
+export const ForgotPasswordDocument = gql`
+    mutation ForgotPassword($email: String!) {
+  forgotPassword(email: $email)
+}
+    `;
+
+export function useForgotPasswordMutation() {
+  return Urql.useMutation<ForgotPasswordMutation, ForgotPasswordMutationVariables>(ForgotPasswordDocument);
 };
 export const LoginDocument = gql`
     mutation Login($username: String!, $password: String!) {
--- src/pages/forgot-password.tsx
+++ src/pages/forgot-password.tsx
@@ -1,11 +1,52 @@
-import React from "react";
+import { Box, Button } from "@chakra-ui/react";
+import { Form, Formik } from "formik";
+import React, { useState } from "react";
+import { InputField } from "../components/InputField";
+import { Wrapper } from "../components/Wrapper";
+import { createUrqlClient } from "../utils/createUrqlClient";
+import { withUrqlClient } from "next-urql";
+import { useForgotPasswordMutation } from "../generated/graphql";
 
 const forgotPassword: React.FC<{}> = ({}) => {
-  return (
-    <>
-      I forgot my password :c
-    </>
-  )
-}
+  const [complete, setComplete] = useState(false);
+  const [, forgotPassword] = useForgotPasswordMutation();
 
-export default forgotPassword;
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ email: "" }}
+        onSubmit={async (values) => {
+          await forgotPassword(values);
+          setComplete(true);
+        }}
+      >
+        {({ isSubmitting }) =>
+          complete ? (
+            <Box>
+              If an account with that email exists, we sent you an email.
+            </Box>
+          ) : (
+            <Form>
+              <InputField
+                name="email"
+                label="Email"
+                placeholder="email@example.com"
+              />
+
+              <Button
+                mt="8px"
+                type="submit"
+                colorScheme="teal"
+                isLoading={isSubmitting}
+              >
+                Forgot password
+              </Button>
+            </Form>
+          )
+        }
+      </Formik>
+    </Wrapper>
+  );
+};
+
+export default withUrqlClient(createUrqlClient)(forgotPassword);
