d1e1105f3408f6b5608ed6ba41a81249931cfad5
Anthony Suárez <neolight1010@gmail.com>
2022-02-15 18:59:08 -0500

    Gen types for new posts return type.

--- src/generated/graphql.ts
+++ src/generated/graphql.ts
@@ -75,6 +75,12 @@ export type MutationLoginArgs = {
   username: Scalars['String'];
 };
 
+export type PaginatedPosts = {
+  __typename?: 'PaginatedPosts';
+  posts: Array<Post>;
+  hasMore: Scalars['Boolean'];
+};
+
 export type Post = {
   __typename?: 'Post';
   id: Scalars['Float'];
@@ -90,7 +96,7 @@ export type Post = {
 export type Query = {
   __typename?: 'Query';
   hello: Scalars['String'];
-  posts: Array<Post>;
+  posts: PaginatedPosts;
   post?: Maybe<Post>;
   me: UserResponse;
 };
@@ -242,10 +248,14 @@ export type PostsQueryVariables = Exact<{
 
 export type PostsQuery = (
   { __typename?: 'Query' }
-  & { posts: Array<(
-    { __typename?: 'Post' }
-    & Pick<Post, 'id' | 'title' | 'textSnippet' | 'createdAt' | 'updatedAt'>
-  )> }
+  & { posts: (
+    { __typename?: 'PaginatedPosts' }
+    & Pick<PaginatedPosts, 'hasMore'>
+    & { posts: Array<(
+      { __typename?: 'Post' }
+      & Pick<Post, 'id' | 'title' | 'textSnippet' | 'createdAt' | 'updatedAt'>
+    )> }
+  ) }
 );
 
 export const UsernameAndIdFragmentDoc = gql`
@@ -349,11 +359,14 @@ export function useMeQuery(options: Omit<Urql.UseQueryArgs<MeQueryVariables>, 'q
 export const PostsDocument = gql`
     query Posts($limit: Int, $cursor: DateTime) {
   posts(limit: $limit, cursor: $cursor) {
-    id
-    title
-    textSnippet
-    createdAt
-    updatedAt
+    posts {
+      id
+      title
+      textSnippet
+      createdAt
+      updatedAt
+    }
+    hasMore
   }
 }
     `;
