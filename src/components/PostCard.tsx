3f14dff56e78014dfc31c87784dc154af5d7bdd4
Anthony Suárez <neolight1010@gmail.com>
2022-08-23 20:42:19 -0500

    Only show edit and delete buttons if user is post's author

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,12 +1,14 @@
 import { Box, Flex, Heading, Text, Link, Spacer } from "@chakra-ui/layout";
-import { PostsQuery, useDeletePostMutation } from "../generated/graphql";
+import { PostsQuery, useDeletePostMutation, useMeQuery } from "../generated/graphql";
 import { VoteSection } from "./VoteSection";
 import NextLink from "next/link";
 import { IconButton } from "@chakra-ui/button";
 import { DeleteIcon, EditIcon, SpinnerIcon } from "@chakra-ui/icons";
 
+type PostsQueryPost = PostsQuery["posts"]["postsWithUserVote"][0];
+
 interface PostCardProps {
-  postWithUserVote: PostsQuery["posts"]["postsWithUserVote"][0];
+  postWithUserVote: PostsQueryPost;
 }
 
 export const PostCard = ({ postWithUserVote }: PostCardProps): JSX.Element => {
@@ -29,11 +31,7 @@ interface PostCardHeaderProps {
 }
 
 const PostCardHeader = ({ post }: PostCardHeaderProps): JSX.Element => {
-  const [{ fetching: deletingPost }, deletePost] = useDeletePostMutation();
-
-  const onDeletePost = () => {
-    deletePost({ id: post.id });
-  };
+  const [{ data: meData }] = useMeQuery();
 
   return (
     <Flex>
@@ -49,19 +47,39 @@ const PostCardHeader = ({ post }: PostCardHeaderProps): JSX.Element => {
 
       <Spacer />
 
-      <NextLink href="/post/edit/[id]" as={`/post/edit/${post.id}`}>
-        <IconButton aria-label="Edit post" mr={2} as={Link}>
-          <EditIcon />
-        </IconButton>
-      </NextLink>
-
-      <IconButton
-        aria-label="Delete post"
-        colorScheme="red"
-        onClick={onDeletePost}
-      >
-        {deletingPost ? <SpinnerIcon /> : <DeleteIcon />}
-      </IconButton>
+      {
+        meData?.me.user?.id === post.author.id
+          ? <AuthorButtons post={post} />
+          : null
+      }
     </Flex>
   );
 };
+
+interface AuthorButtonsProps {
+  post: PostsQueryPost["post"];
+}
+
+const AuthorButtons = ({ post }: AuthorButtonsProps): JSX.Element => {
+  const [{ fetching: deletingPost }, deletePost] = useDeletePostMutation();
+
+  const onDeletePost = () => {
+    deletePost({ id: post.id });
+  };
+
+  return <Box>
+    <NextLink href="/post/edit/[id]" as={`/post/edit/${post.id}`}>
+      <IconButton aria-label="Edit post" mr={2} as={Link}>
+        <EditIcon />
+      </IconButton>
+    </NextLink>
+
+    <IconButton
+      aria-label="Delete post"
+      colorScheme="red"
+      onClick={onDeletePost}
+    >
+      {deletingPost ? <SpinnerIcon /> : <DeleteIcon />}
+    </IconButton>
+  </Box>;
+}
