c16a9e14148c332aa6cc8788d773dd8c89907ca2
Anthony Suárez <neolight1010@gmail.com>
2022-08-23 22:12:52 -0500

    Implement EditPost page

--- src/pages/post/edit/[id].tsx
+++ src/pages/post/edit/[id].tsx
@@ -1,13 +1,43 @@
-import {withUrqlClient} from "next-urql"
-import { Layout } from "../../../components/Layout"
-import {createUrqlClient} from "../../../utils/createUrqlClient"
+import { withUrqlClient } from "next-urql";
+import { useRouter } from "next/router";
+import { CreatePostForm } from "../../../components/CreatePostForm";
+import { Layout } from "../../../components/Layout";
+import { usePostQuery, useUpdatePostMutation } from "../../../generated/graphql";
+import { createUrqlClient } from "../../../utils/createUrqlClient";
+
+const INVALID_POST_ID = "-1";
 
 const EditPost = (): JSX.Element => {
-  return <Layout>
+  const router = useRouter();
 
-    Edit post page
+  const postId: string =
+    typeof router.query.id === "string" ? router.query.id : INVALID_POST_ID;
 
+  const [{ data: postData }] = usePostQuery({
+    variables: { id: postId },
+    pause: postId === INVALID_POST_ID,
+  });
+
+  const [_, updatePost] = useUpdatePostMutation();
+
+  return (
+    <Layout>
+      <CreatePostForm
+        onSubmit={async (values) => {
+          await updatePost({
+            id: postId,
+            title: values.title,
+            text: values.text,
+          });
+        }}
+
+        initialValues={{
+          title: postData?.post?.title || "",
+          text: postData?.post?.text || "",
+        }}
+      />
     </Layout>
-}
+  );
+};
 
-export default withUrqlClient(createUrqlClient)(EditPost)
+export default withUrqlClient(createUrqlClient)(EditPost);
